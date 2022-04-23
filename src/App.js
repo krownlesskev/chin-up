64c689e31ed601f7ec45da6f692aa9d37e074d7e
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 01:36:10 -0500

    added functionality for generating excuse and category

--- src/App.js
+++ src/App.js
@@ -6,21 +6,26 @@ import './styles.css';
 
 
 function App() {
-  const [excuse, setExcuse] = useState();
-  
-  // Fetches data from api and displays in the console
-const handleClick = () => {
-  Axios.get('https://excuser.herokuapp.com/v1/excuse')
-    .then((res) => {
-      console.log(res.data[0].excuse);
-    });
-};
+  const [excuse, setExcuse] = useState('');
+  const [category, setCategory] = useState('');
+
+  // Fetches data from api sets it in state
+  const handleClick = () => {
+    Axios.get('https://excuser.herokuapp.com/v1/excuse')
+      .then((res) => {
+        setExcuse(res.data[0].excuse);
+        setCategory(res.data[0].category);
+      });
+  };
 
   return (
     <div className="container">
       <h1>CHIN UP</h1>
       <p>The Mental Health App</p>
+      {/* Clicking the generate button will display the Excuse and Category */}
       <button onClick={handleClick}>Generate</button>
+      <p>Excuse: {excuse}</p>
+      <p>Category: {category}</p>
     </div>
   );
 }
