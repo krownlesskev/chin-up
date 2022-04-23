5008381398a0c056449bd7873a68b84385599c10
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 01:32:29 -0500

    adding hooks and moved function into app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './styles.css';
-import Axios from 'axios';
+import { useState } from 'react';
 
-// Fetches data from api and displays in the console
-const handleClick = () => {
-  Axios.get('https://excuser.herokuapp.com/v1/excuse')
-  .then((res) => {
-    console.log(res.data[0].excuse);
-  });
-};
+import Axios from 'axios';
+import './styles.css';
+
+
 
 function App() {
+  const [excuse, setExcuse] = useState();
+  
+  // Fetches data from api and displays in the console
+const handleClick = () => {
+  Axios.get('https://excuser.herokuapp.com/v1/excuse')
+    .then((res) => {
+      console.log(res.data[0].excuse);
+    });
+};
+
   return (
     <div className="container">
       <h1>CHIN UP</h1>
+      <p>The Mental Health App</p>
       <button onClick={handleClick}>Generate</button>
     </div>
   );
