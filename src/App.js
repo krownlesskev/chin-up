52ce769f90a28a8020f5c5affe8d8680b830d920
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 12:32:43 -0500

    moved footer to its own component and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExcuseButton from './components/excuses/excuse-button.component';
+import ExcuseButton from './components/excuse-button/excuse-button.component';
+import Footer from './components/footer/footer.component';
 
 import './styles.scss';
 
@@ -15,10 +16,7 @@ function App() {
         <ExcuseButton category='Children' />
         <ExcuseButton category='Party' />
       </div>
-      <footer>
-        <p className='footer-main-text'>I dedicate this application to my love, <span>Michelle</span>.</p>
-        <p className='footer-sub-text'>Who fights every single day to be the strongest woman she already is.</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,12 @@
+import './footer.styles.scss';
+
+const Footer = () => {
+    return (
+        <>
+            <p>I dedicate this application to my love, <span>Michelle</span>.</p>
+        </>
+    );
+};
+
+
+export default Footer;
