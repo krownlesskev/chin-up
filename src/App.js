fecf2515e25f0ee58e2bc0b825e07b33714be364
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 01:29:11 -0500

    added basic functionality for api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './styles.css';
+import Axios from 'axios';
+
+// Fetches data from api and displays in the console
+const handleClick = () => {
+  Axios.get('https://excuser.herokuapp.com/v1/excuse')
+  .then((res) => {
+    console.log(res.data[0].excuse);
+  });
+};
 
 function App() {
   return (
     <div className="container">
       <h1>CHIN UP</h1>
+      <button onClick={handleClick}>Generate</button>
     </div>
   );
 }
