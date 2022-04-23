1bc7c864593f3950c814bc51343d52bdbf7680f4
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 12:25:21 -0500

    created excuse generator button component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { useState } from 'react';
+import ExcuseButton from './components/excuses/excuse-button.component';
 
-import Axios from 'axios';
 import './styles.scss';
 
 
+
 function App() {
-  const [excuse, setExcuse] = useState('');
-  const [category, setCategory] = useState('');
-
-  // Fetches data from api sets it in state
-  const handleClick = () => {
-    Axios.get('https://excuser.herokuapp.com/v1/excuse')
-      .then((res) => {
-        setExcuse(res.data[0].excuse);
-        setCategory(res.data[0].category);
-      });
-  };
-
   return (
     <div className="container">
       <h1>CHIN UP</h1>
       <h2>The Mental Health App</h2>
-      {/* Clicking the generate button will display the Excuse and Category */}
       <div className='excuse-generator'>
-        <button onClick={handleClick}>Generate</button>
-        <p>Excuse: {excuse}</p>
-        <p>Category: {category}</p>
+        <ExcuseButton category='Family' />
+        <ExcuseButton category='Office' />
+        <ExcuseButton category='Children' />
+        <ExcuseButton category='Party' />
       </div>
       <footer>
         <p className='footer-main-text'>I dedicate this application to my love, <span>Michelle</span>.</p>
