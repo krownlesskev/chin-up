2a049557b9f9f0e9be6d3a720328d81ff034d7ba
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 13:01:32 -0500

    refactored component

--- src/components/excuse-button/excuse-button.component.jsx
+++ src/components/excuse-button/excuse-button.component.jsx
@@ -5,27 +5,22 @@ import './excuse-button.styles.scss';
 
 const ExcuseButton = ({ category }) => {
     const [excuse, setExcuse] = useState('');
-    const [buttonCategory, setButtonCategory] = useState('');
     const newCategory = category.toLowerCase();
-    // Fetches data from api sets it in state
-
 
     const handleClick = () => {
         Axios.get(`https://excuser.herokuapp.com/v1/excuse/${newCategory}`)
             .then((res) => {
                 console.log(res);
                 setExcuse(res.data[0].excuse);
-                setButtonCategory(res.data[0].category);
                 console.log(excuse);
             });
     };
 
     return (
-        <>
+        <div className="excuse-button-container">
             <button onClick={handleClick}>{category}</button>
             <p>Excuse:  {excuse}</p>
-            <p>Category:  {buttonCategory}</p>
-        </>
+        </div>
     );
 };
 
