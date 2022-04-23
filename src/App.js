e047ac9eb1a24af3437e0288f96a626e25e12068
Kevin Sana <kevin.sana@outlook.com>
2022-04-23 12:42:21 -0500

    moved header into its own component and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import Header from './components/header/header.component';
 import ExcusesContainer from './components/excuses-container/excuses-container.component';
-
 import Footer from './components/footer/footer.component';
 
 import './styles.scss';
@@ -8,8 +8,7 @@ import './styles.scss';
 function App() {
   return (
     <div className="container">
-      <h1>CHIN UP</h1>
-      <h2>The Mental Health App</h2>
+      <Header />
       <ExcusesContainer />
       <Footer />
     </div>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import './header.styles.scss';
+
+const Header = () => {
+    return (
+        <>
+            <h1>CHIN UP</h1>
+            <h2>The Mental Health App</h2>;
+        </>
+    );
+};
+
+export default Header;
